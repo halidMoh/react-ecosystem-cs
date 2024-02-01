89437c6c270df82812436ef7c41065a493e039f3
Halid Awol <halidm@ksu.edu>
2024-01-31 20:50:15 -0800

    Added persist to retain data after refresh

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 const root = createRoot(document.getElementById("root"));
+
+const theStore = store;
+const persistor = persistStore(theStore);
 root.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={theStore}>
+    <PersistGate loading={<div>Loading</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -1,12 +1,19 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { todos } from "./todos/reducers";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 const reducers = {
   todos,
 };
-
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 const rootReducer = combineReducers(reducers);
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
